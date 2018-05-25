3889a9e30ad7afdd7bb89ff935419a817b1fd248
Maria <mariaanguevara@gmail.com>
2018-05-25 12:31:18 -0500

    subiendo cifrado funcional

--- src/index.js
+++ src/index.js
@@ -8,34 +8,31 @@ function cifrar ()
  
     let texto= [];
     let resultado1= [];
+    let resultadostring="";
    
     numero=posicion.value;
     const infouser =textociuser.value; 
     
-    let asci=infouser.charAt(i).charCodeAt(0);
-    for ( var i = 0; i < infouser.length;i++)
-    
+    let asci;
+    for ( var i=0; i<infouser.length; i++)
     {
-      if( 64<asci || asci<91){
-        texto[i]=((infouser.charAt(i).charCodeAt(0)) - 65 + parseInt(numero))%26 +65;
-        resultado1[i] = String.fromCharCode(texto[i]);
-
+      asci = infouser.charAt(i).charCodeAt(0);
+      if( 64<asci && asci<91) {
+        texto[i] = (asci - 65 + parseInt(numero)) % 26 + 65;
+      } else if(96<asci&&asci<123) {
+        texto[i]=(asci - 97 + parseInt(numero))%26 +97;
+        //resultado1[i] = String.fromCharCode(texto[i]);
+      } else {
+        texto[i]=asci;
       }
-      else if(96<asci||asci<123)
-      {
-         texto[i]=((infouser.charAt(i).charCodeAt(0)) - 97 + parseInt(numero))%26 +97;
-         resultado1[i] = String.fromCharCode(texto[i]);
-
-      }
-  
-     else(65>asci>123)
-     {
-       resultado1[i]=asci;
-        
-     }
-      
     }
-      document.getElementById("resultadocifrar").value= (resultado1[i]); 
+    //console.log("texto",texto)
+    for ( var i=0; i<texto.length; i++) {
+      resultado1[i]=String.fromCharCode(texto[i]);
+    }
+   // console.log("resultado",resultado1)
+    resultadostring=resultado1.join().replace(/^[,]+|[,]+$|[,]+/g, "");
+    document.getElementById("resultadocifrar").value=resultadostring; 
 
 
 
@@ -43,4 +40,39 @@ function cifrar ()
     //document.getElementById("resultadocifrar").value=cadena2.join().replace(/^[, ]+|[, ]+$|[, ]+/g, "");   
  } 
 
-
+ function descifrar ()
+ {
+  
+    let textuser= document.getElementById("textodecif");
+    let posicion= document.getElementById("posiciondedescifrado");
+    let respuesta= document.getElementById("resultadodescifrar").value;
+    let texto= [];
+    let resultado1= [];
+    let resultadostring="";
+    
+    const numero=posicion.value;
+    const infouser =textuser.value; 
+     
+    let asci;
+    for ( var i=0; i<infouser.length; i++)
+     {
+       asci = infouser.charAt(i).charCodeAt(0);
+        if( 64<asci && asci<91) {
+          texto[i] = ((asci - 65 + parseInt(numero)) % 26 + 65)+26;
+         } else if(asci==65){
+          texto[i] = ((asci - 97 + parseInt(numero)) % 26 + 97)+26;  
+         }else if(96<asci&&asci<123) {
+         //resultado1[i] = String.fromCharCode(texto[i]);
+         } else {
+         texto[i]=asci;
+         }console.log("texto",texto)
+     }
+     
+     for ( var i=0; i<texto.length; i++) {
+     resultado1[i]=String.fromCharCode(texto[i]);
+     }
+     //console.log("resultado",resultado1)
+     
+     resultadostring=resultado1.join().replace(/^[,]+|[,]+$|[,]+/g, "");
+     document.getElementById("resultadodescifrar").value=resultadostring; 
+ }
