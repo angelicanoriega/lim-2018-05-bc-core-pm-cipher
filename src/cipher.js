5ca8a582ea37e8bb3dfa5d7a245cd85073130cc3
Maria Noriega <mariaanguevara@gmail.com>
2018-05-29 00:41:57 -0500

    organizando htmal para css 2

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,65 @@
 window.cipher = {
-  // ... 
-};
+   encode: cipher => {
+    const string =document.getElementById("stringcipher").value; 
+    let posicion= document.getElementById("posiciondecifrado");
+    let texto= [];
+    let resultado= [];
+    let cipher="";
+   
+    let numero=parseInt(posicion.value);
+    let encode;  
+     for ( var i=0; i<string .length; i++)
+     {
+       encode = string .charAt(i).charCodeAt(0);
+       if( 64<encode && encode<91) {
+         texto[i] = (encode - 65 + numero) % 26 + 65;
+       } else if(96<encode&&encode<123) {
+         texto[i]=(encode - 97 + numero)%26 +97;
+       }else if(numero==null){
+       decipher="no pusiste un numero en el cuadro posicion";}
+       else {
+         texto[i]=encode;
+       }
+     }
+     
+     for ( var i=0; i<texto.length; i++) {
+       resultado[i]=String.fromCharCode(texto[i]);
+     }
+   
+     cipher=resultado.join().replace(/^[,]+|[,]+$|[,]+/g, "");// ver coma
+     document.getElementById("cipher").value=cipher; 
+   }, 
+   decode: decipher => {
+    const string =document.getElementById("stringdecipher").value; 
+    let posicion= document.getElementById("posiciondedescifrado");
+    let texto= [];
+    let resultado= [];
+    let decipher="";
+   
+    let numero=parseInt(posicion.value);
+    let decode;
+    
+     for ( var i=0; i<string .length; i++)
+     {
+       decode = string .charAt(i).charCodeAt(0);
+       if( 64<decode && decode<91) {
+         texto[i] = (decode - 65 - numero +26) % 26 + 65;
+       } else if(96<decode&&decode<123) {
+         texto[i]=(decode - 97 - numero +26)%26 +97;
+         //resultado1[i] = String.fromCharCode(texto[i]);
+       }else if(numero==null){
+       decipher="no pusiste un numero en el cuadro posicion";
+       }else {
+         texto[i]=decode;
+       }
+     }
+     
+     for ( var i=0; i<texto.length; i++) {
+       resultado[i]=String.fromCharCode(texto[i]);
+     }
+   
+     decipher=resultado.join().replace(/^[,]+|[,]+$|[,]+/g, "");// ver coma
+     document.getElementById("decipher").value=decipher; 
+   }
+
+  }
